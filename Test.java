f9b62936ccba4d58be83944a43457d9d043022be
Annnn <huuan04112002@gmail.com>
2021-12-27 14:24:20 +0700

    Delete Test.java

--- Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        HangManMaterial art = new HangManMaterial();
-        System.out.println(art.getStage(2));
-        System.out.println(art.getRandomWord());
-    }
-}
